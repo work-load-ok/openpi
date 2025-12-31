ff5dc776b9d4162b0517ce9a73c26fb878b3518e
yelan <774309187@qq.com>
2025-12-31 15:14:16 +0800

    增加自定义Pi0config_Custom,继承自原Pi0Config

--- src/openpi/models/pi0_config.py
+++ src/openpi/models/pi0_config.py
@@ -111,23 +111,6 @@ class Pi0Config(_model.BaseModelConfig):
 
 @dataclasses.dataclass(frozen=True)
 class Pi0Config_Custom(Pi0Config):
-    # dtype: str = "bfloat16"
-    # paligemma_variant: _gemma.Variant = "gemma_2b"
-    # action_expert_variant: _gemma.Variant = "gemma_300m"
-
-    # # Set the model specific defaults.
-    # action_dim: int = 32
-    # action_horizon: int = 50
-    
-    # # max_token_len: int = 48
-    # max_token_len: int = None  # type: ignore
-    # # Pi05 has two differences from Pi0:
-    # # - the state input is part of the discrete language tokens rather than a continuous input that is part of the suffix
-    # # - the action expert uses adaRMSNorm to inject the flow matching timestep
-    # pi05: bool = False
-    # # This config option is not used directly by the model, but it is read by the ModelTransformFactory.
-    # discrete_state_input: bool = None  # type: ignore
-
     timestep_difference_mode: bool = False  # * If true, the frame_index input represents the difference between two timesteps.
     stage_process_mode: bool = False
 
@@ -140,82 +123,5 @@ class Pi0Config_Custom(Pi0Config):
     p_mask_ego_state: float = 0.0
     cfg_scale: float = 1.0
 
-    def __post_init__(self):
-        if self.max_token_len is None:
-            object.__setattr__(self, "max_token_len", 200 if self.pi05 else 48)
-        if self.discrete_state_input is None:
-            object.__setattr__(self, "discrete_state_input", self.pi05)  # * discrete_state_input is equal to self.pi05
-            # * self.pi05 with discrete_state_input
-            # * self.pi0, no discrete_state_input
 
-    @property
-    @override
-    def model_type(self) -> _model.ModelType:
-        if self.pi05:
-            return _model.ModelType.PI05
-        return _model.ModelType.PI0
-
-    @override
-    def create(self, rng: at.KeyArrayLike) -> "Pi0":
-        from openpi.models.pi0 import Pi0
-        return Pi0(self, rngs=nnx.Rngs(rng))
-
-    @override
-    def inputs_spec(self, *, batch_size: int = 1) -> tuple[_model.Observation, _model.Actions]:
-        image_spec = jax.ShapeDtypeStruct([batch_size, *_model.IMAGE_RESOLUTION, 3], jnp.float32)
-        image_mask_spec = jax.ShapeDtypeStruct([batch_size], jnp.bool_)
-
-        with at.disable_typechecking():
-            observation_spec = _model.Observation(
-                images={
-                    "base_0_rgb": image_spec,
-                    "left_wrist_0_rgb": image_spec,
-                    "right_wrist_0_rgb": image_spec,
-                },
-                image_masks={
-                    "base_0_rgb": image_mask_spec,
-                    "left_wrist_0_rgb": image_mask_spec,
-                    "right_wrist_0_rgb": image_mask_spec,
-                },
-                # TODO: add history for spec
-                state=jax.ShapeDtypeStruct([batch_size, self.action_dim], jnp.float32),
-                tokenized_prompt=jax.ShapeDtypeStruct([batch_size, self.max_token_len], jnp.int32),
-                tokenized_prompt_mask=jax.ShapeDtypeStruct([batch_size, self.max_token_len], bool),
-                frame_index=jax.ShapeDtypeStruct([batch_size], jnp.int32),  # * Add frame_index to observation spec
-                episode_length=jax.ShapeDtypeStruct([batch_size], jnp.float32),  # * Add episode_length to observation spec
-            )
-        action_spec = jax.ShapeDtypeStruct([batch_size, self.action_horizon, self.action_dim], jnp.float32)
-
-        return observation_spec, action_spec
-
-    def get_freeze_filter(self) -> nnx.filterlib.Filter:
-        """Returns the freeze filter based on the model config."""
-        filters = []
-        has_lora = False
-        gemma_params_filter = nnx_utils.PathRegex(".*llm.*")
-        action_expert_params_filter = nnx_utils.PathRegex(".*llm.*_1.*")
-        if "lora" in self.paligemma_variant:
-            filters.append(
-                gemma_params_filter,
-            )
-            if "lora" not in self.action_expert_variant:
-                # If only freeze gemma params, exclude action expert params.
-                filters.append(
-                    nnx.Not(action_expert_params_filter),
-                )
-            has_lora = True
-        elif "lora" in self.action_expert_variant:
-            filters.append(
-                action_expert_params_filter,
-            )
-            has_lora = True
-
-        if has_lora:
-            # If any lora is used, exclude all lora params. (all loras are trainable)
-            filters.append(
-                nnx.Not(nnx_utils.PathRegex(".*lora.*")),
-            )
-        
-        if not filters:
-            return nnx.Nothing
-        return nnx.All(*filters)
+    
