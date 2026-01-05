26d1d73094f81eba234e1a3e434c0947647c9d3b
yelan <774309187@qq.com>
2026-01-05 16:39:10 +0800

    copy from progress_estimator, instead of PaligemmaTokenizer, add action_advantage in tokenize

--- src/openpi/models/tokenizer.py
+++ src/openpi/models/tokenizer.py
@@ -19,9 +19,16 @@ class PaligemmaTokenizer:
         with path.open("rb") as f:
             self._tokenizer = sentencepiece.SentencePieceProcessor(model_proto=f.read())
 
-    def tokenize(self, prompt: str, state: np.ndarray | None = None) -> tuple[np.ndarray, np.ndarray]:
+    def tokenize(self, prompt: str, state: np.ndarray | None = None, action_advantage: np.ndarray | None = None) -> tuple[np.ndarray, np.ndarray]:
         cleaned_text = prompt.strip().replace("_", " ").replace("\n", " ")
-        if state is not None:
+        if state is not None and action_advantage is not None:
+            # This is the Pi05 format with action quality indicator, where the state is part of the discrete language input.
+            discretized_state = np.digitize(state, bins=np.linspace(-1, 1, 256 + 1)[:-1]) - 1
+            state_str = " ".join(map(str, discretized_state))
+            
+            full_prompt = f"Task: {cleaned_text}, State: {state_str}, Advantage: {action_advantage};\nAction: "
+            tokens = self._tokenizer.encode(full_prompt, add_bos=True)
+        elif state is not None:
             # This is the Pi05 format, where the state is part of the discrete language input.
             discretized_state = np.digitize(state, bins=np.linspace(-1, 1, 256 + 1)[:-1]) - 1
             state_str = " ".join(map(str, discretized_state))
